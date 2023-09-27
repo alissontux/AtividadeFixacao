af878b36a0cc7bd930b5c898d9f7abdf28fb2c8f
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-09-27 11:40:04 -0300

    implementing the model

---
+++ src/main/java/org/example/model/ItemModel.java
@@ -0,0 +1,44 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class ItemModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 200)
+    private String nome;
+    @Column(nullable = false)
+    private double valor;
+
+    public ItemModel(String nome, double valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
