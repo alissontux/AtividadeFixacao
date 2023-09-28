03b7eb5255457cb8e6ab8eda31d1d438ccb8cfbb
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-09-28 16:27:45 -0300

    implementing the service baseCompras and controller baseCompras

--- src/main/java/org/example/controller/RebeldeController.java
+++ src/main/java/org/example/controller/RebeldeController.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import org.example.model.RebeldeModel;
+import org.example.service.BaseCompraService;
 import org.example.service.RebeldeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/rebeldes")
 public class RebeldeController {
     private final RebeldeService rebeldeService;
-
+    private final BaseCompraService baseCompraService;
     @Autowired
-    public RebeldeController(RebeldeService rebeldeService) {
+    public RebeldeController(RebeldeService rebeldeService, BaseCompraService baseCompraService) {
         this.rebeldeService = rebeldeService;
+        this.baseCompraService = baseCompraService;
     }
-
     @PostMapping
     public RebeldeModel adicionarRebeldes(@RequestBody RebeldeRequest rebeldeRequest) {
         return rebeldeService.adicionarRebeldes(rebeldeRequest);
@@ -48,5 +49,15 @@ public class RebeldeController {
         double porcentagemRebeldes = ((double) traidores - totalRebeldes) / totalRebeldes * 100;
 
         return ResponseEntity.ok(porcentagemRebeldes);
+    }
+    @PostMapping("/{id}/comprar")
+    public ResponseEntity<String> comprarItem(@PathVariable Long id, @RequestBody String nome){
+        RebeldeModel rebeldeModel = rebeldeService.obterRebeldePorId(id);
+        boolean compraSucesso = baseCompraService.comprarItem(rebeldeModel,nome);
+        if (compraSucesso ){
+            return ResponseEntity.ok("Compra efetuada com sucesso");
+        }else {
+            return ResponseEntity.badRequest().body("Compra falhou");
+        }
     }
 }
--- src/main/java/org/example/service/ItemService.java
+++ src/main/java/org/example/service/ItemService.java
@@ -1,55 +1,55 @@
-package org.example.service;
-
-import jakarta.persistence.EntityNotFoundException;
-import org.example.model.ItemModel;
-import org.example.model.RebeldeModel;
-import org.example.repository.RebeldeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ItemService {
-
-
-    private final RebeldeRepository rebeldeRepository;
-
-    @Autowired
-    public ItemService(RebeldeRepository rebeldeRepository) {
-        this.rebeldeRepository = rebeldeRepository;
-    }
-
-    public RebeldeModel comprarItem(Long id, String nomeItem) {
-        RebeldeModel rebeldeModel = rebeldeRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Rebelde não encontrado"));
-
-        if (!rebeldeModel.isRebeldeAtivo()) {
-            throw new EntityNotFoundException("Rebelde não pode comprar o item");
-        }
-
-        ItemModel itemSelecionado;
-
-        switch (nomeItem.toLowerCase()) {
-            case "arma":
-                itemSelecionado = new ItemModel("Arma", 100.0);
-                break;
-
-            case "municao":
-                itemSelecionado = new ItemModel("Munição", 30.0);
-                break;
-
-            case "agua":
-                itemSelecionado = new ItemModel("Água", 5.0);
-                break;
-
-            case "comida":
-                itemSelecionado = new ItemModel("Comida", 15.0);
-                break;
-
-            default:
-                throw new EntityNotFoundException("Item não existe");
-        }
-
-        rebeldeModel.adicionarItemAoInventario(itemSelecionado);
-
-        return rebeldeRepository.save(rebeldeModel);
-    }
-}
+//package org.example.service;
+//
+//import jakarta.persistence.EntityNotFoundException;
+//import org.example.model.ItemModel;
+//import org.example.model.RebeldeModel;
+//import org.example.repository.RebeldeRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class ItemService {
+//
+//
+//    private final RebeldeRepository rebeldeRepository;
+//
+//    @Autowired
+//    public ItemService(RebeldeRepository rebeldeRepository) {
+//        this.rebeldeRepository = rebeldeRepository;
+//    }
+//
+//    public RebeldeModel comprarItem(Long id, String nomeItem) {
+//        RebeldeModel rebeldeModel = rebeldeRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Rebelde não encontrado"));
+//
+//        if (!rebeldeModel.isRebeldeAtivo()) {
+//            throw new EntityNotFoundException("Rebelde não pode comprar o item");
+//        }
+//
+//        ItemModel itemSelecionado;
+//
+//        switch (nomeItem.toLowerCase()) {
+//            case "arma":
+//                itemSelecionado = new ItemModel("Arma", 100.0);
+//                break;
+//
+//            case "municao":
+//                itemSelecionado = new ItemModel("Munição", 30.0);
+//                break;
+//
+//            case "agua":
+//                itemSelecionado = new ItemModel("Água", 5.0);
+//                break;
+//
+//            case "comida":
+//                itemSelecionado = new ItemModel("Comida", 15.0);
+//                break;
+//
+//            default:
+//                throw new EntityNotFoundException("Item não existe");
+//        }
+//
+//        rebeldeModel.adicionarItemAoInventario(itemSelecionado);
+//
+//        return rebeldeRepository.save(rebeldeModel);
+//    }
+//}
