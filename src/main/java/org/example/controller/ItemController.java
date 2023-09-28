a49e3912f320bb77be7a790c8398dd04992b6a96
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-09-28 16:30:47 -0300

    implementing the service baseCompras and controller baseCompras

--- src/main/java/org/example/controller/ItemController.java
+++ src/main/java/org/example/controller/ItemController.java
@@ -1,28 +1,28 @@
-//package org.example.controller;
-//
-//import jakarta.persistence.EntityNotFoundException;
-//import org.example.service.ItemService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/itens")
-//public class ItemController {
-//    private final ItemService itemService;
-//    @Autowired
-//
-//    public ItemController(ItemService itemService) {
-//        this.itemService = itemService;
-//    }
-//    @PostMapping("/{id}/comprar")
-//    public ResponseEntity<?> comprarItem(@PathVariable Long id, @RequestBody String nome){
-//        try{
-//            itemService.comprarItem(id, nome);
-//            return new ResponseEntity<>("Item comprado com sucesso", HttpStatus.OK);
-//        }catch (EntityNotFoundException e ){
-//            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
-//        }
-//    }
-//}
+package org.example.controller;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.example.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/itens")
+public class ItemController {
+    private final ItemService itemService;
+    @Autowired
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+    @PostMapping("/{id}/comprar")
+    public ResponseEntity<?> comprarItem(@PathVariable Long id, @RequestBody String nome){
+        try{
+            itemService.comprarItem(id, nome);
+            return new ResponseEntity<>("Item comprado com sucesso", HttpStatus.OK);
+        }catch (EntityNotFoundException e ){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+}
