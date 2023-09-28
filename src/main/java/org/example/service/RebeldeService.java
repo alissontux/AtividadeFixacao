54318195d1d0362cc8acb4216cd3563ae8d91943
vitoria.ferreira <vitoria.ferreira@zup.com.br>
2023-09-28 11:35:25 -0300

    adding tests for rebel service class methods

--- src/main/java/org/example/service/RebeldeService.java
+++ src/main/java/org/example/service/RebeldeService.java
@@ -7,7 +7,7 @@ import org.example.repository.RebeldeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Service
